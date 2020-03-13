c1a69e7d8ceaafa49d9cc8d760fe847c12e6f7ce
Tyler de Friess <tyler.defriess@gmail.com>
2020-03-13 11:19:34 -0400

    built index.js, server.js, added express dependency

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const projectRouter = require('./projects/projectRouter');
+
+const server = express();
+
+server.use(express.json());
+server.use(logger);
+
+server.get('/', (req, res) => {
+    res.send({ message: 'Welcome to my sprint challenge API' });
+});
+
+// server.use('/api/projects', projectRouter);
+// server.use('/api/actions', actionRouter);
+
+function logger(req, res, next) {
+    console.log(`${req.method} to ${req.originalUrl} at ${new Date().toISOString()}`);
+
+    next();
+}
+
+module.exports = server;
